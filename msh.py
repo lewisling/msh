db7795db5c939963bea557544bb805fd6ffd01e5
mlnlbrt <albert.malina@gmail.com>
2015-03-12 21:32:20 +0100

    Initial commandline support in msh.py, small code formatting changes

--- msh.py
+++ msh.py
@@ -1,16 +1,61 @@
 #!/usr/bin/env python
 
+from sys import argv
+import time
+import argparse
 import cv2
 import streamreader
-import time
-from sys import argv
+
+## commandline parsing
+# TODO: write nicer description and helps...
+
+parser = argparse.ArgumentParser(
+	description = '''Main program which perform processing on the given \
+		video stream and send results to stdout.''')
+parser.add_argument(
+	"stream_type",
+	help = "type of a given stream",
+	choices = ["onefile", "multiplefiles", "stream"])
+parser.add_argument(
+	"stream_path",
+	help = "path to the video stream")
+parser.add_argument(
+	"haar_cascade_path",
+	help = "path to XML file which contains trained HaarCascade")
+parser.add_argument(
+	"facerec_method",
+	help = "face recognition method",
+	choices = ["eigenfaces", "fisherfaces"])
+parser.add_argument(
+	"facerec_model_path",
+	help = "path to XML file with model state for chosen \
+		face recognition method")
+parser.add_argument(
+	"-v", "--verbose",
+	help = "enable on-screen informations",
+	action = "store_true")
+parser.add_argument(
+	"-sf", "--scale_factor",
+	help = "one of the face detection algorithm parameters",
+	default = 1.3,
+	type = float)
+parser.add_argument(
+	"-mn", "--min_neighbors",
+	help = "one of the face detection algorithm parameters",
+	default = 5,
+	type = int)
+args = parser.parse_args()
+
 
 ## initializing variables and objects
-stream_path = argv[1]
-haar_cascade_xml_path = argv[2]
 
-face_cascade = cv2.CascadeClassifier(haar_cascade_xml_path)
-stream_reader = streamreader.MultipleFiles(stream_path)
+if args.stream_type == "onefile":
+	stream_reader = streamreader.OneFile(args.stream_path)
+elif args.stream_type == "multiplefiles":
+	stream_reader = streamreader.MultipleFiles(args.stream_path)
+else:
+	stream_reader = streamreader.Stream(args.stream_path)
+face_cascade = cv2.CascadeClassifier(args.haar_cascade_path)
 
 
 while(True):
@@ -22,24 +67,26 @@ while(True):
 		print "End of stream"
 		break
 		
-	faces = face_cascade.detectMultiScale(gray_frame, 2, 5)
-	for (x,y,w,h) in faces:
+	faces = face_cascade.detectMultiScale(
+		gray_frame, args.scale_factor, args.min_neighbors)
+	for (x, y, w, h) in faces:
 		cv2.rectangle(
 		stream_reader.current_frame, 
-		(x, y), (x + w, y + h), (255, 0, 0), 2)
-	
+		(x, y), (x + w, y + h), (255, 255, 255), 2)
+		
 	finish_time = time.time()
 
-	cv2.putText(
-		stream_reader.current_frame, 
-		"FPS: " + str(round(1 / (finish_time - begin_time), 2)), 
-		(0, 12), 
-		cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255), 1)
+	if args.verbose:
+		cv2.putText(
+			stream_reader.current_frame, 
+			"FPS: " + str(round(1 / (finish_time - begin_time), 2)), 
+			(0, 12), 
+			cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255), 1)
 	cv2.imshow("mlnSpyHole - main window", stream_reader.current_frame)
 	
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		cv2.destroyAllWindows()
 		break
-
+	
 		
 print "Exiting..."
--- mshFacerecTrain.py
+++ mshFacerecTrain.py
@@ -3,15 +3,16 @@
 from sys import argv
 import os
 import fnmatch
+import argparse
 import numpy as np
 import cv2
-import argparse
 
 ## commandline parsing
-## TODO: write nicer description and helps...
+# TODO: write nicer description and helps...
+
 parser = argparse.ArgumentParser(
 	description = '''Small utility which trains face recognizer \
-		and save results in XML file. Result file can be used later in \
+		and save model state in XML file. Result file can be used later in \
 		application that rely on Eigenfaces or Fisherfaces.''')
 parser.add_argument(
 	"cropped_faces_dir", 
@@ -26,17 +27,23 @@ parser.add_argument(
 	choices = ["eigenfaces", "fisherfaces"])
 args = parser.parse_args()
 
+
 ## initializing variables
+
 cropped_faces_dir = args.cropped_faces_dir
 train_faces_image = []
 train_faces_index = []
 
+
 ## fixing cropped_faces_dir  
+
 if cropped_faces_dir[len(cropped_faces_dir) - 1] == '/':
 	cropped_faces_dir = cropped_faces_dir[:len(cropped_faces_dir) - 1]
 	
+	
 ## resursive processing of given directory
-## TODO: maybe add support for different types of frames, not only .pgm?
+# TODO: maybe add support for different types of frames, not only .pgm?
+
 for root, dirs, names in os.walk(cropped_faces_dir):
 	if dirs == []:
 		for name in names:
@@ -45,11 +52,12 @@ for root, dirs, names in os.walk(cropped_faces_dir):
 				im = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
 				index = root[root.rfind("/") + 1:]
 				train_faces_image.append(np.asarray(im, dtype = np.uint8))
-				train_faces_index.append(index)
-				
+				train_faces_index.append(index)			
 train_faces_index = np.asarray(train_faces_index, dtype = np.int32)
 
+
 ## training chosen face recognizer
+
 if args.method == "eigenface":
 	face_recognizer = cv2.createEigenFaceRecognizer()
 else:
@@ -58,11 +66,15 @@ face_recognizer.train(
 	np.asarray(train_faces_image), 
 	np.asarray(train_faces_index))	
 	
+	
 ## saving trained set
+
 xmlfile_name = cropped_faces_dir[cropped_faces_dir.rfind("/") + 1:]
 xmlfile_name = xmlfile_name + "-" + args.method
 face_recognizer.save(cropped_faces_dir + "/" + xmlfile_name + ".xml")
 
+
 ## TODO: sending report to stdout
+
 if not args.quiet:
 	print face_recognizer.getInt("ncomponents")

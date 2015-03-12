78b76d83becb03d724e1161e63048f05e287e215
mlnlbrt <albert.malina@gmail.com>
2015-03-12 20:12:47 +0100

    On-screen information about FPS

--- msh.py
+++ msh.py
@@ -2,19 +2,20 @@
 
 import cv2
 import streamreader
+import time
 from sys import argv
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+## initializing variables and objects
 stream_path = argv[1]
 haar_cascade_xml_path = argv[2]
 
 face_cascade = cv2.CascadeClassifier(haar_cascade_xml_path)
 stream_reader = streamreader.MultipleFiles(stream_path)
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 while(True):
+	begin_time = time.time()
+	
 	try:
 		gray_frame = stream_reader.read()
 	except:
@@ -26,11 +27,19 @@ while(True):
 		cv2.rectangle(
 		stream_reader.current_frame, 
 		(x, y), (x + w, y + h), (255, 0, 0), 2)
-  
+	
+	finish_time = time.time()
+
+	cv2.putText(
+		stream_reader.current_frame, 
+		"FPS: " + str(round(1 / (finish_time - begin_time), 2)), 
+		(0, 12), 
+		cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255), 1)
 	cv2.imshow("mlnSpyHole - main window", stream_reader.current_frame)
 	
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		cv2.destroyAllWindows()
 		break
+
 		
 print "Exiting..."

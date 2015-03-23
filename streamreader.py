441c9779aac17528ee44913cbcebc29ffdf3c62f
mlnlbrt <albert.malina@gmail.com>
2015-03-23 16:29:38 +0100

    Added main() in streamreader.py

--- streamreader.py
+++ streamreader.py
@@ -26,6 +26,7 @@ class MultipleFiles(StreamReader):
 	def __init__(self, path):
 		super(MultipleFiles, self).__init__(path)
 		print "Stream type: multiplefiles"
+		print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 		self._frames_paths = iter(sorted(glob.glob(self.path + "*.jpg")))
 		
 	def __del__(self):
@@ -43,6 +44,7 @@ class OneFile(StreamReader):
 	def __init__(self, path):
 		super(OneFile, self).__init__(path)
 		print "Stream type: onefile"
+		print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 		
 	def __del__(self):
 		super(OneFile, self).__del__()
@@ -59,6 +61,7 @@ class Stream(StreamReader):
 	def __init__(self, path):
 		super(Stream, self).__init__(path)
 		print "Stream type: stream"
+		print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 		self._stream = cv2.VideoCapture(self.path)
 		
 	def __del__(self):
@@ -68,3 +71,67 @@ class Stream(StreamReader):
 		_, self.current_frame = self._stream.read()
 		self.gray_frame = cv2.cvtColor(self.current_frame, cv2.COLOR_BGR2GRAY)
 		return self.gray_frame
+
+if __name__ == "__main__":
+	import sys
+	import argparse
+	import time
+	
+	def main():
+		## commandline parsing
+		# TODO: write nicer description and helps...
+		
+		parser = argparse.ArgumentParser(
+			description = '''Class used to abstract video sequence source''')
+		parser.add_argument(
+			"stream_type",
+			help = "type of a given stream",
+			choices = ["onefile", "multiplefiles", "stream"])
+		parser.add_argument(
+			"stream_path",
+			help = "path to the video stream")
+		parser.add_argument(
+			"-o", "--on_screen_info",
+			help = "enable on-screen informations",
+			action = "store_true")
+		args = parser.parse_args()
+		
+		
+		if args.stream_type == "onefile":
+			stream_reader = OneFile(args.stream_path)
+		elif args.stream_type == "multiplefiles":
+			stream_reader = MultipleFiles(args.stream_path)
+		elif args.stream_type == "stream":
+			stream_reader = Stream(args.stream_path)
+		else:
+			pass
+			
+		frame_time = sys.float_info.max
+		while(True):
+			begin_time = time.time()
+			
+			try:
+				stream_reader.read()
+			except:
+				print "End of stream"
+				break
+			
+			if args.on_screen_info:
+				cv2.putText(
+					stream_reader.current_frame, 
+					"FPS: " + str(round(1 / frame_time, 1)), 
+					(0, 12), 
+					cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255), 1)
+			cv2.imshow("mlnSpyHole - main window", stream_reader.current_frame)
+			
+			frame_time = time.time() - begin_time
+			
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				cv2.destroyAllWindows()
+				break
+				
+				
+		print "Exiting..."
+		
+		
+	main()

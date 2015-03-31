da26c55fdb9764c9807be40ae2c92902896b5e68
mlnlbrt <albert.malina@gmail.com>
2015-03-31 18:38:56 +0200

    Make mshGrabber running without error, added cmdline argument descibing person id in sequence, added cmdline arguments checking, added saving grabbed frame to target directory under name based on current date and time

--- mshGrabber.py
+++ mshGrabber.py
@@ -2,6 +2,8 @@
 
 import sys
 import os
+import time
+import datetime
 import argparse
 import cv2
 import streamreader
@@ -10,7 +12,8 @@ import streamreader
 # TODO: write nicer description and helps...
 
 parser = argparse.ArgumentParser(
-	description = '''Class used to abstract video sequence source''')
+	description = '''Small utility to grab frames from given stream \
+		and save them in a specified dir''')
 parser.add_argument(
 	"stream_type",
 	help = "type of a given stream",
@@ -22,6 +25,10 @@ parser.add_argument(
 	"target_path",
 	help = "path to a directory where grabbed frames will be written")
 parser.add_argument(
+	"identifier",
+	help = "id of person in sequence",
+	type = int)
+parser.add_argument(
 	"-o", "--on_screen_info",
 	help = "enable on-screen informations",
 	action = "store_true")
@@ -32,13 +39,21 @@ parser.add_argument(
 	type = float)
 args = parser.parse_args()
 
+if args.identifier <= 0:
+	print "Person id should be > 0"
+	exit()
+	
+if args.max_fps <= 0:
+	print "FPS limiter should be set at value > 0"
+	exit()
+
 
 ## initializing variables and objects
 
 if args.stream_type == "onefile":
-	stream_reader = OneFile(args.stream_path, args.max_fps)
+	stream_reader = streamreader.OneFile(args.stream_path, args.max_fps)
 elif args.stream_type == "stream":
-	stream_reader = Stream(args.stream_path, args.max_fps)
+	stream_reader = streamreader.Stream(args.stream_path, args.max_fps)
 else:
 	pass
 	
@@ -53,6 +68,7 @@ if not os.path.exists(args.target_path):
 		print "Can't mkdir target directories"
 		exit()
 
+person_id = str(args.identifier).rjust(4, '0')
 
 ## main loop with frame grabbing
 
@@ -66,7 +82,12 @@ while(True):
 		print "Something went wrong with stream"
 		break
 		
-	# TODO: grabbed frame saving
+	date = datetime.datetime.now()
+	cv2.imwrite(
+		args.target_path + 
+		person_id + 
+		date.strftime("-%Y_%m_%d-%H_%M_%S_%f") + ".jpg", 
+		stream_reader.current_frame)
 	
 	if args.on_screen_info:
 		cv2.putText(

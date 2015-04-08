84ecdd5b6ca6bd9cd2b07e687639b2f54eee4146
mlnlbrt <albert.malina@gmail.com>
2015-04-08 16:50:44 +0200

    Now mshCropper works only on sequences with xml groundtruth file assigned to them, deleted some unnecessary cmdline arguments in mshCropper

--- mshCropper.py
+++ mshCropper.py
@@ -24,12 +24,8 @@ parser.add_argument(
 	help = "path to a directory where cropped faces will be written in \
 		different subdirectories for each id")
 parser.add_argument(
-	"id_type",
-	help = "choose sequence type",
-	choices = ["xml", "person"])
-parser.add_argument(
 	"id_path",
-	help = "path to xml with id information or person id")
+	help = "path to xml with id information or groundtruth")
 parser.add_argument(
 	"face_cascade_path",
 	help = "path to face HaarCascade")
@@ -52,14 +48,6 @@ parser.add_argument(
 	help = "determines percentage of eyes width in cropped image width",
 	default = 0.67,
 	type = float)
-parser.add_argument(
-	"-t", "--output_type",
-	help = "output images type",
-	default = "pgm",
-	choices = ["pgm", "png", "bmp"])
-parser.add_argument(
-	"-pf", "--prefix",
-	help = "prefix added to output files")
 parser.add_argument(
 	"-he", "--histogram_equalization",
 	help = "enable histogram equalization of output images",
@@ -87,33 +75,34 @@ if not os.path.exists(args.target_path):
 # it assumed that frame contains at most one face
 # in case of id_type = person eye coords are filled with "-1"
 frames_info = []
-if args.id_type == "xml":
-	try:
-		xmltree = xmlreader.parse(args.id_path)
-	except:
-		print "ID file cannot be opened"
-		exit()
-	dataset = xmltree.getroot()
-	for frame in dataset:
-		if len(frame) == 1:
-			frame_name = frame.get("number")
-			for person in frame:
-				if len(person) == 2:
-					person_id = person.get("id")
-					eye_coord = []
-					for eyes in person:
-						eye_coord.append(
-							(int(eyes.get('x')), int(eyes.get('y'))))
-					frames_info.append([
-						args.stream_path + frame_name + ".jpg", 
-						person_id, eye_coord[0], eye_coord[1]])
-elif args.id_type == "person":
-	frames_paths = sorted(glob.glob(args.stream_path + "*.jpg"))
-	for frame_path in frames_paths:
-		frames_info.append([frame_path, args.id_path, (-1, -1), (-1, -1)])
-else:
-	pass
-	
+try:
+	xmltree = xmlreader.parse(args.id_path)
+except:
+	print "ID file cannot be opened"
+	exit()
+dataset = xmltree.getroot()
+for frame in dataset:
+	if len(frame) == 1:
+		frame_name = frame.get("number")
+		for person in frame:
+			person_id = person.get("id")
+			# groundtruth from chokepoint
+			if len(person) == 2:
+				eye_coord = []
+				for eyes in person:
+					eye_coord.append(
+						(int(eyes.get('x')), int(eyes.get('y'))))
+				frames_info.append([
+					args.stream_path + frame_name + ".jpg", 
+					person_id, eye_coord[0], eye_coord[1]])
+			# "groundtruth" created by mshGrabber
+			elif len(person) == 0:
+				frames_info.append([
+					args.stream_path + frame_name + ".jpg", 
+					person_id, (-1, -1), (-1, -1)])
+			else:
+				pass
+
 try:
 	face_cropper = facecropper.FaceCropper(
 		args.face_cascade_path, args.eyepair_cascade_path,
@@ -126,27 +115,18 @@ except:
 	
 target_image_size = (args.cropped_image_size, args.cropped_image_size)
 
-if args.prefix:
-	files_prefix = args.prefix
-else:
-	stream_path = args.stream_path[:len(args.stream_path) - 1]
-	stream_name = stream_path[stream_path.rfind("/") + 1:]
-	files_prefix = stream_name
+stream_path = args.stream_path[:len(args.stream_path) - 1]
+stream_name = stream_path[stream_path.rfind("/") + 1:]
+files_prefix = stream_name
 
 
 ## checking for subjects directories existence in target_path
 ## and possibly creating them if not exists
 
 try:
-	if args.id_type == "xml":
-		for _, person_id, _, _ in frames_info:
-			if not os.path.exists(args.target_path + person_id):
-				os.mkdir(args.target_path + person_id)
-	elif args.id_type == "person":
-		if not os.path.exists(args.target_path + args.id_path):
-			os.mkdir(args.target_path + args.id_path)
-	else:
-		pass
+	for _, person_id, _, _ in frames_info:
+		if not os.path.exists(args.target_path + person_id):
+			os.mkdir(args.target_path + person_id)
 except:
 	print "Can't mkdir target directories"
 	exit()

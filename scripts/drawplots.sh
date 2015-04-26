c1d86d543ddfaa609a45531892bed1fb2d7eb34e
mlnlbrt <albert.malina@gmail.com>
2015-04-26 21:31:12 +0200

    WIP version of drawplots

--- scripts/drawplots.sh
+++ scripts/drawplots.sh
@@ -24,11 +24,11 @@ case "$#" in
 		;;
 esac
 
+resolutions="800x600"
+
 # TODO: face detection plots
-#~ for resolution in $resolutions
-for resolution in "800x600"
+for resolution in $resolutions
 do
-	> $tests_path/results-facedetector-$resolution
 	for sf in $face_cascade_sfs
 	do
 		for mn in $face_cascade_mns
@@ -36,39 +36,54 @@ do
 			cat $tests_path/results_facedetector-$resolution-* | \
 			awk -vsf="$sf" -vmn="$mn" \
 			'{if($4 == sf && $5 == mn) \
-				{groundtruth_faces+=$2; \
-					correct_faces+=$6; incorrect_faces+=$7; \
-					fps+=$31; num+=1;}} \
-				END {print sf, mn, \
-					groundtruth_faces, correct_faces+incorrect_faces, \
-					correct_faces, incorrect_faces, fps/num}' \
-					>> $tests_path/results-facedetector-$resolution
+				{groundtruth_faces+=$2; correct_faces+=$6;}} \
+				END {printf "detected_faces %s|%s %s\n", 
+					sf, mn, \
+					(correct_faces/groundtruth_faces)*100}'
+			cat $tests_path/results_facedetector-$resolution-* | \
+			awk -vsf="$sf" -vmn="$mn" \
+			'{if($4 == sf && $5 == mn) \
+				{correct_faces+=$6; incorrect_faces+=$7;}} \
+				END {printf "incorrect_detections %s|%s %s\n", 
+					sf, mn, \
+					(incorrect_faces/(correct_faces+incorrect_faces))*100}'
 		done
-	done
-	# detected correctly vs present in sequence
-	cat $tests_path/results-facedetector-$resolution | \
-	awk '{print $2, $1, ($5/$3)*100}' | \
-	./utils/plots2d.py \
-	"min neighbors" "scale factor" "correct detections [%]" -r 0.1
-	#~ # detected incorrectly vs detected in general
-	cat $tests_path/results-facedetector-$resolution | \
-	awk '{print $2, $1, ($6/$4)*100}' | \
-	./utils/plots2d.py \
-	"min neighbors" "scale factor" "errors [%]" -r 0.1
-	# detection speed (fps), mean for each scale factor
+	done | ./utils/plots2d.py \
+	"" "scale factor|min neighbors" \
+	"detected faces | incorrect detections [%]" \
+	-r 0.1 -d -c "gr"
+done 
+echo "--------"
+# detection speed (fps), mean for each scale factor
+for resolution in $resolutions
+do
 	for sf in $face_cascade_sfs
 	do
-		cat $tests_path/results-facedetector-$resolution | \
+		# min fps
+		cat $tests_path/results_facedetector-$resolution-* | \
 		awk -vsf="$sf" \
-		'{if($1 == sf) {fps+=$7; num+=1;}} END {print 0, sf, fps/num}'
-	done | ./utils/plots2d.py "min neighbors" "scale factor" "fps" -r 0.1
-done 
+		'{if($4 == sf) \
+			{fps+=$29; num+=1;}} \
+			END {printf "min %s %s\n", sf, fps/num}'
+		# max fps
+		cat $tests_path/results_facedetector-$resolution-* | \
+		awk -vsf="$sf" \
+		'{if($4 == sf) \
+			{fps+=$30; num+=1;}} \
+			END {printf "max %s %s\n", sf, fps/num}'
+		# avg fps
+		cat $tests_path/results_facedetector-$resolution-* | \
+		awk -vsf="$sf" \
+		'{if($4 == sf) \
+			{fps+=$31; num+=1;}} \
+			END {printf "avg %s %s\n", sf, fps/num}'
+	done | ./utils/plots2d.py "" "scale factor" "fps" -r 0.1 -p1r -d -lp 2
+done
+echo "--------"
 
 # TODO: eyepair detection plots
-#~ for resolution in $resolutions
-for resolution in "800x600"
+for resolution in $resolutions
 do
-	> $tests_path/results-eyepairdetector-$resolution
 	for sf in $eyepair_cascade_sfs
 	do
 		for mn in $eyepair_cascade_mns
@@ -76,40 +91,54 @@ do
 			cat $tests_path/results_eyepairdetector-$resolution-* | \
 			awk -vsf="$sf" -vmn="$mn" \
 			'{if($9 == sf && $10 == mn) \
-				{groundtruth_faces+=$2; \
-					correct_faces+=$11; incorrect_faces+=$12; \
-					fps+=$31; num+=1;}} \
-				END {print sf, mn, \
-					groundtruth_faces, correct_faces+incorrect_faces, \
-					correct_faces, incorrect_faces, fps/num}' \
-					>> $tests_path/results-eyepairdetector-$resolution
+				{groundtruth_faces+=$2; correct_faces+=$11;}} \
+				END {printf "detected_faces %s|%s %s\n", 
+					sf, mn, \
+					(correct_faces/groundtruth_faces)*100}'
+			cat $tests_path/results_eyepairdetector-$resolution-* | \
+			awk -vsf="$sf" -vmn="$mn" \
+			'{if($9 == sf && $10 == mn) \
+				{correct_faces+=$11; incorrect_faces+=$12;}} \
+				END {printf "incorrect_detections %s|%s %s\n", 
+					sf, mn, \
+					(incorrect_faces/(correct_faces+incorrect_faces))*100}'
 		done
-	done
-	#~ # detected correctly vs present in sequence
-	cat $tests_path/results-eyepairdetector-$resolution | \
-	awk '{print $2, $1, ($5/$3)*100}' | \
-	./utils/plots2d.py \
-	"min neighbors" "scale factor" "correct detections [%]" -r 0.1
-	# detected incorrectly vs detected in general
-	cat $tests_path/results-eyepairdetector-$resolution | \
-	awk '{print $2, $1, ($6/$4)*100}' | \
-	./utils/plots2d.py \
-	"min neighbors" "scale factor" "errors [%]" -r 1.0
-	#~ # detection speed (fps), mean for each scale factor
+	done | ./utils/plots2d.py \
+	"" "scale factor|min neighbors" \
+	"detected faces | incorrect detections [%]" \
+	-r 0.1 -d -c "gr"
+done 
+echo "--------"
+# detection speed (fps), mean for each scale factor
+for resolution in $resolutions
+do
 	for sf in $eyepair_cascade_sfs
 	do
-		cat $tests_path/results-eyepairdetector-$resolution | \
+		# min fps
+		cat $tests_path/results_eyepairdetector-$resolution-* | \
 		awk -vsf="$sf" \
-		'{if($1 == sf) {fps+=$7; num+=1;}} END {print 0, sf, fps/num}'
-	done | ./utils/plots2d.py "min neighbors" "scale factor" "fps" -r 0.5
-done 
+		'{if($9 == sf) \
+			{fps+=$29; num+=1;}} \
+			END {printf "min %s %s\n", sf, fps/num}'
+		# max fps
+		cat $tests_path/results_eyepairdetector-$resolution-* | \
+		awk -vsf="$sf" \
+		'{if($9 == sf) \
+			{fps+=$30; num+=1;}} \
+			END {printf "max %s %s\n", sf, fps/num}'
+		# avg fps
+		cat $tests_path/results_eyepairdetector-$resolution-* | \
+		awk -vsf="$sf" \
+		'{if($9 == sf) \
+			{fps+=$31; num+=1;}} \
+			END {printf "avg %s %s\n", sf, fps/num}'
+	done | ./utils/plots2d.py "" "scale factor" "fps" -r 0.1 -p1r -d -lp 2
+done
+echo "--------"
 
 # TODO: face recognition plots
-#~ eyes_positions="0.2 0.25 0.3"
-#~ eyes_widths="0.7 0.8 0.9"
 # classification efficiency
 #~ for resolution in $resolutions
-#~ for resolution in "800x600"
 #~ do
 	#~ for method in $facerec_methods
 	#~ do
@@ -121,15 +150,17 @@ done
 				#~ awk -vep="$ep" -vew="$ew" -vmethod="$method" \
 				#~ '{if($14 == method && $15 == ep && $16 == ew) \
 					#~ {sum1+=$17; sum2+=($17+$18);}} \
-					#~ END {print ew, ep, sum1/sum2}'
+					#~ END {printf "%s %s,%s %s\n", \
+						#~ method, ew, ep, (sum1/sum2)*100}'
 			#~ done
-		#~ done | ./utils/plots2d.py \
-		#~ "eyes width" "eyes position" "efficiency - $method [%]"
-	#~ done
+		#~ done 
+	#~ done | ./utils/plots2d.py \
+	#~ "" "eyes width,eyes position" "efficiency [%]" -d -r 0.01
 #~ done
+#~ echo "--------"
+
 # classification speed (fps)
 #~ for resolution in $resolutions
-#~ for resolution in "800x600"
 #~ do
 	#~ for method in $facerec_methods
 	#~ do
@@ -138,18 +169,43 @@ done
 		#~ awk -vresolution="$resolution" -vmethod="$method" \
 		#~ '{if($14 == method) \
 			#~ {fps+=$29; num+=1;}} \
-			#~ END {printf "min %s/%s %s\n", resolution, method, fps/num}'
+			#~ END {printf "min %s|%s %s\n", resolution, method, fps/num}'
 		#~ # max fps
 		#~ cat $tests_path/results_recognizer-$resolution-* | \
 		#~ awk -vresolution="$resolution" -vmethod="$method" \
 		#~ '{if($14 == method) \
 			#~ {fps+=$30; num+=1;}} \
-			#~ END {printf "max %s/%s %s\n", resolution, method, fps/num}'
+			#~ END {printf "max %s|%s %s\n", resolution, method, fps/num}'
 		#~ # avg fps
 		#~ cat $tests_path/results_recognizer-$resolution-* | \
 		#~ awk -vresolution="$resolution" -vmethod="$method" \
 		#~ '{if($14 == method) \
 			#~ {fps+=$31; num+=1;}} \
-			#~ END {printf "avg %s/%s %s\n", resolution, method, fps/num}'
+			#~ END {printf "avg %s|%s %s\n", resolution, method, fps/num}'
 	#~ done
-#~ done | ./utils/plots2d.py "type" "resolution/method" "fps" -r 0.1 -lp 2 -p1r
+#~ done | ./utils/plots2d.py "type" "resolution|method" "fps" -r 0.1 -lp 2 -p1r
+for resolution in $resolutions
+do
+	for method in $facerec_methods
+	do
+		# min fps
+		cat $tests_path/results_recognizer-$resolution-* | \
+		awk -vresolution="$resolution" -vmethod="$method" \
+		'{if($14 == method) \
+			{fps+=$29; num+=1;}} \
+			END {printf "min %s %s\n", method, fps/num}'
+		# max fps
+		cat $tests_path/results_recognizer-$resolution-* | \
+		awk -vresolution="$resolution" -vmethod="$method" \
+		'{if($14 == method) \
+			{fps+=$30; num+=1;}} \
+			END {printf "max %s %s\n", method, fps/num}'
+		# avg fps
+		cat $tests_path/results_recognizer-$resolution-* | \
+		awk -vresolution="$resolution" -vmethod="$method" \
+		'{if($14 == method) \
+			{fps+=$31; num+=1;}} \
+			END {printf "avg %s %s\n", method, fps/num}'
+	done
+done | ./utils/plots2d.py "type" "method" "fps" -r 0.1 -lp 2 -p1r -d
+echo "--------"
